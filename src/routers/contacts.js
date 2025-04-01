e61cc45f98393baa277ec34c2a9f8db9c91daccc
Andrey Isenko <aisenko87@gmail.com>
2025-04-01 22:36:26 +0300

    add repo-node

---
+++ src/routers/contacts.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { getContactsController, getContactsByIdController } from '../controllers/contacts.js';
+
+const contactsRouter = Router();
+
+contactsRouter.get('/', getContactsController);
+
+contactsRouter.get('/:contactId', getContactsByIdController);
+export default contactsRouter;
