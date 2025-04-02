7367416ee994f3b7bcf714dbd24b1a1cf06daf6a
Andrey Isenko <aisenko87@gmail.com>
2025-04-02 08:22:50 +0300

    add wrapper

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,51 +1,41 @@
-
 import createHttpError from 'http-errors';
 import { getContacts, getContactsById } from '../services/contacts.js';
 
 export const getContactsController = async (req, res) => {
-  try {
-    const data = await getContacts();
+  const data = await getContacts();
 
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data,
+  });
 };
 
 export const getContactsByIdController = async (req, res) => {
-  try {
-    const { contactId } = req.params;
+  const { contactId } = req.params;
 
-    const data = await getContactsById(contactId);
+  const data = await getContactsById(contactId);
 
-    if (!data) {
+  if (!data) {
+    throw createHttpError(404, `Contact not foundssssss`);
+    // const error = new Error(`Contact not foundssssss  `);
+    // error.status = 404;
+    // throw error;
 
-      throw createHttpError(404, `Contact not foundssssss`)
-      // const error = new Error(`Contact not foundssssss  `);
-      // error.status = 404;
-      // throw error;
-
-      // return res.status(404).json({
-      //   status: 404,
-      //   message: 'Contact not found',
-      // });
-    }
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data,
-    });
-  } catch(error) {
-    const { status = 500, message = "Server error" } = error;
-    res.status(status).json({
-      message: error.message,
-    });
+    // return res.status(404).json({
+    //   status: 404,
+    //   message: 'Contact not found',
+    // });
   }
+
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data,
+  });
+
+  // const { status = 500, message = "Server error" } = error;
+  // res.status(status).json({
+  //   message: error.message,
+  // });
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { getContactsController, getContactsByIdController } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import {
+  getContactsController,
+  getContactsByIdController,
+} from '../controllers/contacts.js';
 
 const contactsRouter = Router();
 
-contactsRouter.get('/', getContactsController);
+contactsRouter.get('/', ctrlWrapper(getContactsController));
 
-contactsRouter.get('/:contactId', getContactsByIdController);
+contactsRouter.get('/:contactId', ctrlWrapper(getContactsByIdController));
 export default contactsRouter;
